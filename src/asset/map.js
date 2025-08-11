a00c6dbfa2ff17b1bf39799f6fa2555aec920225
chemilk02 <chemilk02@gmail.com>
2025-08-11 14:55:10 +0900

    map update

--- src/asset/map.js
+++ src/asset/map.js
@@ -1,14 +1,25 @@
-function Map() {
+import React, { useState } from "react";
+import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
+
+function KakaoMap() {
+  const [loading, error] = useKakaoLoader({
+    appkey: process.env.REACT_APP_KAKAO_KEY,
+  });
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+
   return (
       <div className="article">
         <div className="article-title">
             <h3>주변 나눔 매장</h3>
             <button><h5>더보기</h5></button>
         </div>
-        <div className="map-container">
-        </div>
+        <Map center= {{ lat: 37.5665, lng: 126.9780 }} level= {3} className="map-container">
+          <MapMarker position={{ lat: 37.5665, lng: 126.9780 }}><div>서울</div></MapMarker>
+        </Map>
       </div>
   );
 }
 
-export default Map;
+export default KakaoMap;

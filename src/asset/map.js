90bc6689a95c765445e95976ba6d7d94dc49cdc8
chemilk02 <chemilk02@gmail.com>
2025-08-14 00:33:14 +0900

    map update

--- src/asset/map.js
+++ src/asset/map.js
@@ -1,24 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
 
 function KakaoMap() {
-  const apiUrl = process.env.REACT_APP_KAKAO_KEY;
-  const [loading, error] = useKakaoLoader({appkey: apiUrl,});
+  const [loading, error] = useKakaoLoader({ appkey: process.env.REACT_APP_KAKAO_KEY });
 
   if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error!</div>;
+  if (error) return <div>Error loading Kakao SDK!</div>;
 
+  // SDK가 반드시 먼저 로드되어, 이후 Map 컴포넌트 렌더링
   return (
-      <div className="article">
-        <div className="article-title">
-            <h3>주변 나눔 매장</h3>
-            <button><h5>더보기</h5></button>
-        </div>
-        <Map center= {{ lat: 37.5665, lng: 126.9780 }} level= {3} className="map-container" style={{width: 353, height: 160}}>
-          <MapMarker position={{ lat: 37.5665, lng: 126.9780 }}><div>서울</div></MapMarker>
-        </Map>
-      </div>
+    <Map center={{ lat: 37.5665, lng: 126.9780 }} level={3} style={{ width: 353, height: 160 }}>
+      <MapMarker position={{ lat: 37.5665, lng: 126.9780 }}>서울</MapMarker>
+    </Map>
   );
 }
-
-export default KakaoMap;

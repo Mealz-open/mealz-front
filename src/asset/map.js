193317bc0db93f46b8cc3016ef64052c1fae6cb1
chemilk02 <chemilk02@gmail.com>
2025-08-14 01:18:42 +0900

    osm

--- src/asset/map.js
+++ src/asset/map.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
-
-function KakaoMap() {
-  const [loading, error] = useKakaoLoader({ appkey: process.env.REACT_APP_KAKAO_KEY });
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error loading Kakao SDK!</div>;
-
-  // SDK가 반드시 먼저 로드되어, 이후 Map 컴포넌트 렌더링
+import { MapContainer, TileLayer } from "react-leaflet"
+import L from "leaflet"
+import "leaflet/dist/leaflet.css"
+// 초기 중심 위치 설정
+const position = [36.17, 127.83]
+// 최대 경계 설정
+const bounds = L.latLngBounds(
+  [32.5, 123.5], // 남서 좌표 (제주 남서쪽)
+  [39.0, 132.0] // 북동 좌표 (강원도 북동쪽)
+)
+function App() {
   return (
-    <Map center={{ lat: 37.5665, lng: 126.9780 }} level={3} style={{ width: 353, height: 160 }}>
-      <MapMarker position={{ lat: 37.5665, lng: 126.9780 }}>서울</MapMarker>
-    </Map>
-  );
+    <div>
+      <MapContainer
+        center={position} // 초기 중심 좌표
+        zoom={8.0} // 초기 줌 레벨
+        zoomSnap={0.5} // 줌 레벨 스냅
+        maxBounds={bounds} // 최대 경계 설정
+        maxBoundsViscosity={1.0} // 경계의 견고 정도 제어 (1.0일 경우 완전히 견고해져 경계 밖으로 드래그 불가)
+        style={{ width: "353px", height: "160px" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          maxZoom={20} // 최대 줌 레벨
+          minZoom={8.0} // 최소 줌 레벨
+        />
+      </MapContainer>
+    </div>
+  )
 }
-
-export default KakaoMap;
+export default App
--- src/page/home.js
+++ src/page/home.js
@@ -1,7 +1,7 @@
 import SearchBar from '../asset/searchbar.js';
 import NanumType from '../asset/nanum-type.js';
 import Today from '../asset/today.js';
-import KakaoMap from '../asset/map.js';
+import Map from '../asset/map.js';
 import Cert from '../asset/certified.js';
 
 import bannerImg from '../asset/image/img-banner.png';
@@ -15,7 +15,7 @@ function Home() {
             </div>
             <NanumType />
             <Today />
-            <KakaoMap />
+            <Map />
             <Cert />
         </>
     );
